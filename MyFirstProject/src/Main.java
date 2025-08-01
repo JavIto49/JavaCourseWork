36417f901dbe121f6d85287fccb0a89ec4d5d36c
Javito49 <josephvillalobos49@gmail.com>
2025-07-31 20:05:02 -0500

    Day 1 Java refresh

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,211 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //----------------------------------------------------------------
+    /*    // Print statements
+        System.out.println("Hello!");
+        System.out.println("I like pizza, it's really good");
+        System.out.println("Buy me pizza!");
+
+        System.out.println();
+        // 2 Steps to creating a variable
+        int age; // Declaration
+        int miles = 21; // Declaration and assignment
+        int attendees = 50;
+
+        System.out.println(miles);
+
+        System.out.println("I drove about " + miles + " miles today!");
+
+        double price = 499.99;
+        double gpa = 3.5;
+        double temperature = -12.5;
+
+        System.out.println("The Nintendo Switch 2 costs: $" + price);
+        System.out.println();
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println(grade);
+        System.out.println("You need a grade of an " + grade + " to earn an honor award.");
+        System.out.println();
+
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+        System.out.println(isStudent);
+
+        if(isStudent){
+            System.out.println("You are a student");
+        }
+        else{
+            System.out.println("You are not a student");
+        }
+
+        String name = "Joseph Villalobos";
+        String food = "Pizza";
+        String email = "merk1the1game@gmail.com";
+        System.out.println("Hello " + name);
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String enteredName = scanner.nextLine();
+        System.out.println("Welcome " + enteredName + "!");
+        System.out.println();
+
+        System.out.print("Please enter your age: ");
+        int enteredAge = scanner.nextInt();
+        System.out.println("You are " + enteredAge + " years old.");
+        System.out.println();
+
+        System.out.print("Please enter your GPA: ");
+        double enteredGPA = scanner.nextDouble();
+        System.out.println("You GPA is: " + enteredGPA);
+        System.out.println();
+
+        System.out.print("Are you an honors society member? (true/false): ");
+        boolean isHonorSociety = scanner.nextBoolean();
+        if(isHonorSociety){
+            System.out.println("You are an honor society member!");
+        }
+        else{
+            System.out.println("You are not an honor society member.");
+        }
+
+        scanner.close();*/
+        //----------------------------------------------------------------
+
+        /*// Calculate the area of a rectangle
+        double width = 0.0;
+        double height = 0.0;
+        double area = 0.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.print("The area of the rectangle is: " + area + "cm^2");
+
+        scanner.close();*/
+        //----------------------------------------------------------------
+
+        /*// MAD LIBS GAME
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+
+        System.out.print("Enter a noun (animal): ");
+        noun1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+
+        System.out.println("\nToday I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println("The " + noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+        scanner.close();
+        */
+        //----------------------------------------------------------------
+
+        /*// Arithmetic Operators
+        int x = 10;
+        int y = 2;
+
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+
+        a = x + y;
+        b = x - y;
+
+        c = x * y;
+        d = x / y;
+
+        e = x % y;
+
+        System.out.println("Result of a: " + a + "\nResult of b: " + b);
+        System.out.println("Result of c: " + c + "\nResult of d: " + d);
+        System.out.println("Result of e: " + e);
+        System.out.println();
+
+        // Augmented Assignment Operators
+        int f = 10;
+        int g = 20;
+
+        f += g;
+        //f -= g;
+        //f *= g;
+        //f /= g;
+        //f %= g;
+
+        System.out.println(f);
+
+        // Increment and Decrement Operators
+        int j = 5;
+        j--;
+        j++;
+        System.out.println(j);
+
+        // Order of Operations [P-E-M-D-A-S]
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);*/
+        //----------------------------------------------------------------
+
+        /*// SHOPPING CART PROGRAM
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like?: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();*/
+        //----------------------------------------------------------------
+
+        // IF STATEMENTS
+
+    }
+}
